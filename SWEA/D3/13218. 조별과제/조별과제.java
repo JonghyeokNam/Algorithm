b4e70e7b04a07e7deacb251827489679dd01b706
JonghyeokNam <109861353+JonghyeokNam@users.noreply.github.com>
2025-04-30 18:42:53 +0900

    [D3] Title: 조별과제, Time: 92 ms, Memory: 26,624 KB -BaekjoonHub

---
+++ SWEA/D3/13218. 조별과제/조별과제.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            int N = Integer.parseInt(br.readLine());
+
+            int value = N / 3;
+            
+            System.out.println("#" + (i + 1) + " " + value);
+        }
+    }
+}
