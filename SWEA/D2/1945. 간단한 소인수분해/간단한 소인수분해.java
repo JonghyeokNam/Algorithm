7fd9d8606cafeb1054975dc89aae885798585e66
JonghyeokNam <109861353+JonghyeokNam@users.noreply.github.com>
2025-04-30 17:04:35 +0900

    [D2] Title: 간단한 소인수분해, Time: 96 ms, Memory: 27,008 KB -BaekjoonHub

---
+++ SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.java
@@ -0,0 +1,82 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.*;
+import java.io.FileInputStream;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		/*
+		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+		*/
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+		
+			int N = sc.nextInt();
+            
+            int count2 = 0;
+            int count3 = 0;
+            int count5 = 0;
+            int count7 = 0;
+            int count11 = 0;
+            
+            while(N % 2 == 0) {
+            	N /= 2;
+                count2++;
+            }
+            while(N % 3 == 0) {
+            	N /= 3;
+                count3++;
+            }
+            while(N % 5 == 0) {
+            	N /= 5;
+                count5++;
+            }
+            while(N % 7 == 0) {
+            	N /= 7;
+                count7++;
+            }
+            while(N % 11 == 0) {
+            	N /= 11;
+                count11++;
+            }
+
+            System.out.println("#" + test_case + " " + count2 + " " + count3 + " " + count5 + " " + count7 + " " + count11);
+		}
+	}
+}
