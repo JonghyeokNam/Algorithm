8522a3dc7a8d27aeab92683c5bd6c4fe55992f9c
JonghyeokNam <109861353+JonghyeokNam@users.noreply.github.com>
2025-05-02 18:11:07 +0900

    [D2] Title: 패턴 마디의 길이, Time: 75 ms, Memory: 25,088 KB -BaekjoonHub

---
+++ SWEA/D2/2007. 패턴 마디의 길이/패턴 마디의 길이.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= T; i++) {
+            String input = br.readLine();
+
+            char ch = input.charAt(0);
+            int cnt = 1;
+
+            for (int j = 1; j <= 10; j++) {
+                if (ch == input.charAt(j)) {
+                    Boolean isRepeat = true;
+                    for (int k = 0; k < 30 - j; k++) {
+                        if (input.charAt(k) != input.charAt(k + j)) {
+                            isRepeat = false;
+                            break;
+                        }
+                    }
+                    if (isRepeat) {
+                        cnt = j;
+                        break;
+                    }
+                }
+            }
+            System.out.println("#" + i + " " + cnt);
+        }
+    }
+}
